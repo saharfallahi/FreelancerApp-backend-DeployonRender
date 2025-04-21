d15d9524630bef3b16020b860bfc178031f130cd
saharF <s.fallahi.66@gmail.com>
2025-04-21 11:18:37 +0330

    edit sameSite

--- utils/functions.js
+++ utils/functions.js
@@ -29,7 +29,8 @@ async function setAccessToken(res, user) {
     maxAge: 1000 * 60 * 60 * 24 * 1, // would expire after 1 days
     httpOnly: true, // The cookie only accessible by the web server
     signed: true, // Indicates if the cookie should be signed
-    sameSite: "Lax",
+    // sameSite: "Lax",
+    sameSite: "None",
     secure: process.env.NODE_ENV === "development" ? false : true,
     domain: process.env.DOMAIN,
     // domain:process.env.NODE_ENV === "development" ? "localhost" : ".fronthooks.ir",
@@ -46,7 +47,8 @@ async function setRefreshToken(res, user) {
     maxAge: 1000 * 60 * 60 * 24 * 365, // would expire after 1 year
     httpOnly: true, // The cookie only accessible by the web server
     signed: true, // Indicates if the cookie should be signed
-    sameSite: "Lax",
+    // sameSite: "Lax",
+    sameSite: "None",
     secure: process.env.NODE_ENV === "development" ? false : true,
     domain: process.env.DOMAIN,
     // domain:
